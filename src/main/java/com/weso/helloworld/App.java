28f18fbaf445e03fe0375a26e22c282f93993da4
DragonDreamer <luochong@whu.edu.cn>
2021-06-03 18:25:49 +0800

    Initial commit

---
+++ src/main/java/com/weso/helloworld/App.java
@@ -0,0 +1,25 @@
+package com.weso.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
