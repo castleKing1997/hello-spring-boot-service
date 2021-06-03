d0f87fa6d66e32e0e1de7e613c87beb0ce89be22
DragonDreamer <luochong@whu.edu.cn>
2021-06-03 19:53:00 +0800

    URL传参
    URL后加?name=xxx
    现实Hello xxx!

--- src/main/java/com/weso/helloworld/App.java
+++ src/main/java/com/weso/helloworld/App.java
@@ -1,25 +1,21 @@
 package com.weso.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Hello world!
- *
- */
+@SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class App {
-
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
