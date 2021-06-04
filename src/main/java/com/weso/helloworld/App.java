27ea44e7f6aa44e19783f771667a6a11d720613c
DragonDreamer <luochong@whu.edu.cn>
2021-06-04 10:50:16 +0800

    使用RESTful的Web服务
    在java中访问https://quoters.apps.pcfone.io/api/random中的随机quotation。

--- src/main/java/com/weso/helloworld/App.java
+++ src/main/java/com/weso/helloworld/App.java
@@ -1,14 +1,22 @@
 package com.weso.helloworld;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.weso.helloworld.consumingrest.Quote;
 
 @SpringBootApplication
 @RestController
 public class App {
+	private static final org.slf4j.Logger log = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
@@ -17,5 +25,18 @@ public class App {
 	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 }
